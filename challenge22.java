35d67b9383777ec8c18cd80bf45627e29634d0a0
HARSH JANGHEL <harshjanghel875@gmail.com>
2025-05-30 13:22:29 +0530

    Add files via upload

---
+++ challenge22.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class challenge22 {
+  public static void main(String[] args) {
+    Scanner input=new Scanner(System.in);
+    System.out.print("enter number1:");
+    int number1=input.nextInt();
+    System.out.print("enter number2:");
+    int number2=input.nextInt();
+    int result=number1 | number2;
+    System.out.println("RESULT OF BITWISE | OPERATOR:"+result);
+  }
+}
