d6f6d138eb14dae3ab60c44757550e38ab5b0dcd
HARSH JANGHEL <harshjanghel875@gmail.com>
2025-06-07 16:12:33 +0530

    Add files via upload

---
+++ NFE.java
@@ -0,0 +1,8 @@
+class NFE {
+  public static void main(String[] args) {
+    String str = "abc";
+    int num = Integer.parseInt(str); // This will throw NumberFormatException
+    System.out.println(num);
+  }
+  
+}
